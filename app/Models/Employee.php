77927abef84e5aeffce7d30479f65f3350ec5e40
manish-wac <manish.t@webandcrafts.in>
2021-07-26 09:07:32 +0530

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Department;
+use App\Designation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +14,20 @@ class Employee extends Model
     protected $fillable = [
         'name', 'email', 'photo', 'password', 'address', 'dept_id', 'design_id'
     ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+
 }
 
